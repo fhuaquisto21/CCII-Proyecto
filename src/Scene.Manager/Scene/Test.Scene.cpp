cffd790872559a53a5b86c4c2d98db24097d5c5b
fhuaquisto21 <fhuaquisto.aqp@gmail.com>
2022-07-08 15:06:23 -0500

    fix

--- src/Scene.Manager/Scene/Test.Scene.cpp
+++ src/Scene.Manager/Scene/Test.Scene.cpp
@@ -4,7 +4,7 @@ TestScene::TestScene(sf::RenderWindow *_window): Scene("none", _window) {
     //std::string h = "Main Character";
     std::cout << gm::Object::BobCharacter << std::endl;
     this->addObject(this->getMode(), "player_1_character", gm::Object::BobCharacter);
-    this->addObject("gun_weapon", gm::Object::GunWeapon );
+    //this->addObject("gun_weapon", gm::Object::GunWeapon );
     //this->addObject("player_2_character", gm::Object::BobCharacter);
     //this->addCamera("player_1_camera", sf::FloatRect(0.f, 0.f, (float)(_window->getSize().x), (float)(_window->getSize().y)));
     //this->getObject("Main Character");
