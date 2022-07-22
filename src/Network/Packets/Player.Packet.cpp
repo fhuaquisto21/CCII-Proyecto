b994939b39b6a00c807141c5a3c615eb5404066b
fhuaquisto21 <fhuaquisto.aqp@gmail.com>
2022-07-21 19:40:06 -0500

    update

--- src/Network/Packets/Player.Packet.cpp
+++ src/Network/Packets/Player.Packet.cpp
@@ -1,10 +1,10 @@
 #include "./Player.Packet.h"
 
-sf::Packet& operator <<(sf::Packet& packet, const PlayerPacket& m)
+sf::Packet &operator<<(sf::Packet &packet, const PlayerPacket &m)
 {
     return packet << m.name << m.instruction << m.value;
 }
-sf::Packet& operator >>(sf::Packet& packet, PlayerPacket& m)
+sf::Packet &operator>>(sf::Packet &packet, PlayerPacket &m)
 {
     return packet >> m.name >> m.instruction >> m.value;
 }
